3bb71bcc4dc1aa0286a954863e5a63db982b31ce
Andrey DALI <daandrey485@gmail.com>
2023-03-04 22:36:16 +0300

    second prog summa

--- Lesson_1/Exemple003_summa/Program.cs
+++ Lesson_1/Exemple003_summa/Program.cs
@@ -1,8 +1,6 @@
-﻿int nubmer1 = new Random().Next(5,9);
-//случайное число от 5 до 9
+﻿int nubmer1 = new Random().Next(5,9);//случайное число от 5 до 9
 Console.WriteLine(nubmer1);
 int nubmer2 = new Random().Next(5,9);
 Console.WriteLine(nubmer2);
-int resault = nubmer1 + nubmer2;
-// если переменные сложили они уже не выводяться !!!
+int resault = nubmer1 + nubmer2;  // если переменные сложили они уже не выводяться !!!
 Console.WriteLine(resault);
