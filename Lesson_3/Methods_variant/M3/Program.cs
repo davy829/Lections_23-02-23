1ac831bfb7c47782831f0307adaf6ab7acef9447
Andrey DALI <daandrey485@gmail.com>
2023-03-04 23:57:08 +0300

    Method_2 variant no arg, retirn date or random

---
+++ Lesson_3/Methods_variant/M3/Program.cs
@@ -0,0 +1,9 @@
+﻿// ВИД 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int res = Method3(); // вызов здесь ! аргументов нет но вернет через ретерн год, дату, рандом ...
+Console.WriteLine(res);
