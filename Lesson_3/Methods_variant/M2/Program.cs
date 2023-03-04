273832562784a5502f6d9d81de429c9a4c1887a5
Andrey DALI <daandrey485@gmail.com>
2023-03-04 23:56:19 +0300

    Method_2 variant yes arg, no retirn

---
+++ Lesson_3/Methods_variant/M2/Program.cs
@@ -0,0 +1,18 @@
+﻿// вид 2
+void Method2(string msg)
+{
+    Console.WriteLine($" {msg}  @Autor...");
+}
+Method2("New text:"); // вызов метода , аргументы есть - ничего не возвращает !!!
+
+// вид 2.1
+void Method21(string msg, int count)
+{
+    int i = 1;
+    while (i < count)
+    {
+        Console.WriteLine($" {i} - {msg} - {count - i} @Autor...");
+        i++;
+    }
+}
+Method21(msg: "New text:", count: 5); // вызов метода , аргументы есть именованые - ничего не возвращает !!!
