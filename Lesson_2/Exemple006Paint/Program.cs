a64b4cef1e31a39f22e4477a5f1f4d1924a2e1c7
Andrey DALI <daandrey485@gmail.com>
2023-03-04 22:39:04 +0300

    second prog paint +

--- Lesson_2/Exemple006Paint/Program.cs
+++ Lesson_2/Exemple006Paint/Program.cs
@@ -1,21 +1,15 @@
 ﻿Console.Clear();  // чистим консоль
-
 int xa = 45, ya = 5,
     xb = 5, yb = 40,
     xc = 90, yc = 40; // определяем три точки в виде треугольника
-
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
-
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
-
 Console.SetCursorPosition(xc, yc); // выводим треугольник
 Console.WriteLine("+");   // из плюсов
-
 int x = xa, y = xb;  
 int count = 0;
-
 while (count < 100000) //выполним 10000 раз
 {
 int num = new Random().Next(0, 3);  //произвольное число от 0 до 2 (3 не дает)
@@ -34,7 +28,6 @@ if (num == 2)
     x = (x + xc) / 2; //точка икс равна х+90 делить на два
     y = (y + yc) / 2; //точка икс равна у+40 делить на два
 }
-
 Console.SetCursorPosition(x,y); //рисуем все это 
 Console.WriteLine("+");
 count++;
