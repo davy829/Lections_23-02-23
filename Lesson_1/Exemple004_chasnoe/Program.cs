5d25169fe5dc63665f8ba935122dc317d51915f5
Andrey DALI <daandrey485@gmail.com>
2023-03-04 22:36:40 +0300

    second prog chasnoe

--- Lesson_1/Exemple004_chasnoe/Program.cs
+++ Lesson_1/Exemple004_chasnoe/Program.cs
@@ -1,5 +1,4 @@
-﻿double nubmer1 = 15;
-//большой тип данных для деления с остатком
+﻿double nubmer1 = 15;  //большой тип данных для деления с остатком
 double nubmer2 = 2;
 double resault = nubmer1 / nubmer2;
 Console.WriteLine(resault);
