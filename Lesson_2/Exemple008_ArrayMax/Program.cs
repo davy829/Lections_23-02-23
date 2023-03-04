85488aaa7dcc8d142b99519b8edb76bcf64180d7
Andrey DALI <daandrey485@gmail.com>
2023-03-04 22:42:14 +0300

    4 prog function array

--- Lesson_2/Exemple008_ArrayMax/Program.cs
+++ Lesson_2/Exemple008_ArrayMax/Program.cs
@@ -1,13 +1,13 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿
+int Max(int arg1, int arg2, int arg3)
 {
     int resault = arg1;
     if (arg2 > resault) resault = arg2;
     if (arg3 > resault) resault = arg3;
     return resault;
 }
-int[] massiv = { 12, 27, 83, 48, 5, 66, 75, 82, 93 };
-int max = Max(Max(massiv[0],massiv[1],massiv[2]),
-              Max(massiv[3],massiv[4],massiv[5]),
-              Max(massiv[6],massiv[7],massiv[8]));
-
+int[] MyArray = { 12, 27, 83, 48, 5, 66, 75, 82, 93 };
+int max = Max(Max(MyArray[0],MyArray[1],MyArray[2]),
+              Max(MyArray[3],MyArray[4],MyArray[5]),
+              Max(MyArray[6],MyArray[7],MyArray[8]));
 Console.WriteLine(max);
