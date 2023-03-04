35436b48b4338bd7fe72be8afd577f3fc3a0c017
Andrey DALI <daandrey485@gmail.com>
2023-03-04 23:46:48 +0300

    Lectoin_3 Cikle for

---
+++ Lesson_3/For_cicle/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string text)
+{
+    string res = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+    res = res + text;
+    }
+return res;
+}
+ 
+string resault = Method4(text: "z", count: 5); // ловим в ресаулт работу метода , передаем ему аргументы
+Console.Write(resault);
