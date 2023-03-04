18844b26e982fd306e2804b225756763be0576c6
Andrey DALI <daandrey485@gmail.com>
2023-03-04 22:42:55 +0300

    5 prog function find in  array

--- Lesson_2/Exemple009_ArrayFind/Program.cs
+++ Lesson_2/Exemple009_ArrayFind/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] massiv = { 12, 27, 83, 48, 5, 66, 75, 82, 93 };
+﻿// find in array
+int[] massiv = { 12, 27, 83, 48, 5, 66, 75, 82, 93 };
 int count = massiv.Length;
 int find = 93;
 int index = 0;
