31e94414d9e8cca3102eedae2d2cbaae5375b14c
Andrey DALI <daandrey485@gmail.com>
2023-03-04 23:57:46 +0300

    Method_2 variant yes arg, retirn yes

---
+++ Lesson_3/Methods_variant/M4/Program.cs
@@ -0,0 +1,16 @@
+﻿// вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string res = string.Empty;
+    while (i < count)
+    {
+     res = res + text;
+     i++;
+    }
+    return res;
+}
+ 
+string resault = Method4(text:"z",count:5); // ловим в ресаулт работу метода , передаем ему аргументы
+ Console.Write(resault);
