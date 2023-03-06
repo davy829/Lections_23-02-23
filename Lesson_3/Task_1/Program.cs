1440e61f12f3a4434969c747db9185eabc12d069
Andrey DALI <daandrey485@gmail.com>
2023-03-06 23:43:15 +0300

    find and chanch simbols in text

---
+++ Lesson_3/Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+using System;
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
+ + "вы бы взяли приступом согласие прусского короля."
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+string  ChangSim(string text, char oldVal, char newVal)
+{
+    string res = string.Empty;
+    int len = text.Length;
+    for (int i = 0; i < len; i++)
+    {
+        if (text[i] == oldVal)
+        {
+            res = res + $"{newVal}";
+             }
+        else
+                res = res + $"{text[i]}";
+        
+    }
+       return res;
+  }
+
+string NewText = ChangSim(text, ' ', '|');
+Console.Write(NewText);
+Console.WriteLine("//");
+string NewText2 = ChangSim(NewText, 'с', 'С');
+Console.Write(NewText2);
