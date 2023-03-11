dd6f3e2e9886bed7bbe54da2ec6afe19be17c5a4
Andrey DALI <daandrey485@gmail.com>
2023-03-11 22:15:59 +0300

    REcurs fibonachi

--- Lesson_4/fibonachi/Program.cs
+++ Lesson_4/fibonachi/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// fibonachi
+
+
+//----------------------------------------
+// fibo(1) = 1
+// fibo(2) = 1
+// fibo(n) = (n - 1) + (n - 2)
+//-------------------------------------
+
+
 Console.WriteLine("Hello, World!");
+double Fibonachi(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"f{i} -> {Fibonachi(i)}");
+}
