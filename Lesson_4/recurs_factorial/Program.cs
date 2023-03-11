886a7812889cbe77117b0e2b5142575a8cb9a36d
Andrey DALI <daandrey485@gmail.com>
2023-03-11 22:00:03 +0300

    REcurs factorial

---
+++ Lesson_4/recurs_factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+double Factorial(int num)
+{
+    if (num == 1) return 1;
+    else return num * Factorial(num-1);
+}
+
+
+
+for (int i = 1; i < 30; i++)
+{
+  Console.WriteLine($"{i}! {Factorial(i)}");
+}
